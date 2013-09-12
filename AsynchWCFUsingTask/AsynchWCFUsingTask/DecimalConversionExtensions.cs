ae2a43661bcb4754063d7a78cb9857b5cc03d4e0
Nate Adams <psiberknetic@gmail.com>
2013-09-12 18:19:40 -0500

    Added WCF Task based example

---
+++ AsynchWCFUsingTask/AsynchWCFUsingTask/DecimalConversionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsynchWCFUsingTask
+{
+    public static class DecimalConversionExtensions
+    {
+        public static IEnumerable<Decimal> ConvertToDecimalList(this string inputText)
+        {
+            try
+            {
+                string[] values = inputText.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                return values.Select(Decimal.Parse);
+            }
+            catch
+            {
+                Console.WriteLine("An error occurred trying to process the input: " + inputText);
+                return new List<decimal>();
+            }
+        }
+    }
+}
---
+++ AsynchWCFUsingTask/AsynchWCFUsingTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Threading.Tasks;
+using AsynchWCFUsingTask.AdditionService;
+
+namespace AsynchWCFUsingTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var service = new AdditionServiceClient();
+
+            Console.WriteLine("Starting");
+            while (true)
+            {
+                string value = Console.ReadLine();
+                service.AddNumbersAsync(value.ConvertToDecimalList().ToList()).ToObservable()
+                       .Subscribe(x => Console.WriteLine(string.Format("Sum of {0} is {1}", value, x)));
+            }
+        }
+
+        
+    }
+}
